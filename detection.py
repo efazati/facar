bae427c5d0171b916aa010a77743d0f2f934c1a6
efazati <mohammad@efazati.org>
2017-07-18 13:47:36 +0430

    add wrapper over face rec

---
+++ detection.py
@@ -0,0 +1,51 @@
+import face_recognition
+images_path = 'db/image_set/'
+known_images_path = images_path + 'known/'
+unknown_images_path = images_path + 'unknown/'
+
+def enocde_face(path):
+    loaded_image = face_recognition.load_image_file(im)
+    face_encoding = face_recognition.face_encodings(loaded_image)[0]
+    
+    return face_encoding
+
+
+def load_faces(images):
+    result = []
+    for im in images:
+        result.append(enocde_face(im))
+ 
+    return result
+
+def build_db():
+
+    db = []
+    for item in os.listdir(known_images_path):
+        item_path = os.path.join(known_images_path, item)
+        if os.path.isdir(item_path):
+            images = {'name': item, 'files': [], 'encoded': []}
+            for file in os.listdir(item_path):
+                file_path = os.path.join(item_path, file)
+                images['files'].append(file_path)
+            images['encoded'] = load_faces(images['files'])
+            db.append(images)
+    return db
+
+def compare(db, path):
+    unknown = enocde_face(os.path.join(unknown_images_path, path))
+    names = []
+    for item in db:
+        results = face_recognition.compare_faces(item['encoded'], unknown)
+        if True in results:
+            names.append(item['name'])
+    return names
+
+def main():
+    db = build_db
+    for item in os.listdir(unknown_images_path):
+        item_rec_faces = compare(db, item)
+        print (item, '->', item_rec_faces)
+
+
+if __name__ == "__main__": 
+    main()
